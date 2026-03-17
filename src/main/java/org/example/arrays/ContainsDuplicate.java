d9cf2a351a826c2f4bc311531a38f179cfb2a4c2
digigrowth-co <digigrowth39@gmail.com>
2026-03-17 21:31:20 +0530

    contains duplicate 217 leetcode problem

---
+++ src/main/java/org/example/arrays/ContainsDuplicate.java
@@ -0,0 +1,26 @@
+package org.example.arrays;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> set=new HashSet<>();
+
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+
+        public static void main(String[] args) {
+            ContainsDuplicate cd = new ContainsDuplicate();
+            int[] nums = {1, 2, 3, 4, 5, 1};
+            boolean result = cd.containsDuplicate(nums);
+            System.out.println("Contains duplicate: " + result);
+        }
+}
