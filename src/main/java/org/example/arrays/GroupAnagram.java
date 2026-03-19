b6c71a57c42b283654999e6d220cda330981e139
digigrowth-co <digigrowth39@gmail.com>
2026-03-19 20:22:55 +0530

    group anagram

---
+++ src/main/java/org/example/arrays/GroupAnagram.java
@@ -0,0 +1,36 @@
+package org.example.arrays;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagram {
+
+
+        public static List<List<String>> groupAnagrams(String[] strs) {
+            Map<String, List<String>> map = new HashMap<>();
+
+            for (String s : strs) {
+                int[] count = new int[26];
+
+                for (char c : s.toCharArray()) {
+                    count[c - 'a']++;
+                }
+
+                StringBuilder key = new StringBuilder();
+                for (int num : count) {
+                    key.append(num).append("#");
+                }
+
+                map.computeIfAbsent(key.toString(), k -> new ArrayList<>()).add(s);
+            }
+
+            return new ArrayList<>(map.values());
+        }
+        public static void main(String[] args){
+          String[]  strs =new String[] {"eat","tea","tan","ate","nat","bat"};
+          System.out.println(groupAnagrams(strs));
+        }
+
+}
